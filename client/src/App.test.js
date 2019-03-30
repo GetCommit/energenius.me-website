c615d6bacc6d23cb00feb44f2c036136de6be72d
Wenyuan Wu <wenyuanwu@wireless-10-147-126-4.public.utexas.edu>
2019-03-29 20:59:10 -0500

    add frontend tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,109 @@
-import React from '../react';
-import ReactDOM from '../react-dom';
+import React from 'react';
+import { expect } from 'chai';
+import Enzyme from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import "isomorphic-fetch";
+Enzyme.configure({ adapter: new Adapter() });
+
+import Home from './pages/home';
+import Navigation from './pages/Navigation';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe('IndexContent Component', () => {
+    it('Contains Jumbotron', () => {
+      const components = shallow(<Home />);
+      expect(components.find('Jumbotron'));
+    })
+})
+
+describe('IndexContent Component', () => {
+    it('Contains carousel slide', () => {
+      const components = shallow(<Home />);
+      expect(components.find('Carousel'));
+    })
+})
+
+describe('IndexContent Component', () => {
+    it('Contains Carousel Inner', () => {
+      const components = shallow(<Home />);
+      expect(components.find('Carousel.Item'));
+    })
+})
+
+describe('App Component and Classes', () => {
+    it('Contains a Router Component', () => {
+      const about = shallow(<App />)
+      expect(about.find('Router'))
+    })
+})
+
+describe('App', function() {
+    describe('App Components & classes', () => {
+      assert.equal(1, 0)
+
+      it('contains a Router Component', () => {
+        const about = shallow(<App />)
+        expect(about.find('Router'))
+      })
+
+      it('contains a Component', () => {
+        const about = shallow(<App />)
+        expect(about.find('Navigation'))
+      })
+
+      it('contains a Component', () => {
+        const about = shallow(<App />)
+        expect(about.find('Home'))
+      })
+
+      it('contains a Component', () => {
+        const about = shallow(<App />)
+        expect(about.find('Energy'))
+      })
+
+      it('contains a Component', () => {
+        const about = shallow(<App />)
+        expect(about.find('EnergyInstance'))
+      })
+
+      it('contains a Component', () => {
+        const about = shallow(<App />)
+        expect(about.find('Production'))
+      })
+
+      it('contains a Component', () => {
+        const about = shallow(<App />)
+        expect(about.find('ProductionInstance'))
+      })
+
+      it('contains a Component', () => {
+        const about = shallow(<App />)
+        expect(about.find('Country'))
+      })
+
+      it('contains a Component', () => {
+        const about = shallow(<App />)
+        expect(about.find('CountryInstance'))
+      })
+
+      it('contains a Component', () => {
+        const about = shallow(<App />)
+        expect(about.find('About'))
+      })
+
+      it('contains a Component', () => {
+        const about = shallow(<App />)
+        expect(about.find('Footer'))
+      })
+    })
 });
+
+
+describe('Navigation Component', () => {
+  it('contains Navbar', () => {
+    const components = shallow(<Navigation />);
+    expect(components.find('Navigation'));
+  })
+})
