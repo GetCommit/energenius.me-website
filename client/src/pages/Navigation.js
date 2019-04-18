c1db23f93e63f830aa8e04738d024d6373a361f1
Wenyuan Wu <wenyuanwu@wireless-10-147-97-201.public.utexas.edu>
2019-04-17 20:51:06 -0500

    initial progress on search. EST: 10 hrs

--- client/src/pages/Navigation.js
+++ client/src/pages/Navigation.js
@@ -1,31 +1,54 @@
 import React, { Component } from 'react'
-import {Navbar,Nav,NavItem, NavDropdown, Form, FormControl, Button, NavbarBrand} from 'react-bootstrap'
-import {IndexLinkContainer, LinkContainer} from 'react-router-bootstrap'
+import {Navbar,Nav, Form, FormControl, Button, NavbarBrand} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
-import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
-import Home from './home'
-import Energy from './energy'
-import Production from './production'
-import Country from './country'
-import About from './about'
-import Footer from './footer'
+const formValid = ({ formErrors, ...rest }) => {
+  let valid = true;
 
+  // validate form errors being empty
+  Object.values(formErrors).forEach(val => {
+    val.length > 0 && (valid = false);
+  });
+
+  // validate the form was filled out
+  Object.values(rest).forEach(val => {
+    val === null && (valid = false);
+  });
+
+  return valid;
+};
 
 export default class Navigation extends Component {
   constructor(props) {
     super(props)
-    this.state = {name: "Default"}
-    this.textInput = React.createRef();
-
+    this.state = {
+      query: null,
+      formErrors: {
+        query: ""
+      }
+    };
   }
 
   handleSubmit = e => {
     e.preventDefault();
-    this.setState({ value: this.textInput.value})
+
+    if (formValid(this.state)) {
+      console.log(`
+        --SUBMITTING--
+        Query: ${this.state.query}
+      `);
+    } else {
+      console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+    }
   };
+  handleChange = e => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    let formErrors = { ...this.state.formErrors };
 
-
+    // this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+  };
+  
   render(){
     return (
       <Nav class="navbar navbar-light" style={{ backgroundColor: '#e3f2fd' }}>
@@ -54,15 +77,24 @@ export default class Navigation extends Component {
             </Nav>
 
             <Nav fill className="justify-content-end" alignRight>
-              <Form inline className="justify-content-left col-xs-6" onSubmit={this.handleSubmit} >
-              <input type="text" ref={this.textInput} />
-                <Link to={'/search/'+this.textInput.value}>
+              <Form  onSubmit={this.handleSubmit} noValidate inline className="justify-content-left col-xs-6" alignRight >
+                <FormControl 
 
+                  placeholder=""
+                  type="text"
+                  name="query"
+                  noValidate
+                  onChange={this.handleChange}
+
+                />
+
+                <Link to={'/search/'+this.state.query}>
 
                   <Button variant="outline-primary" className="mt-2 mt-sm-0">
                     Search
                   </Button>
-                </Link>
+
+                </Link> 
               </Form>
             </Nav>
 
--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,65 +1,62 @@
 import React, { Component } from 'react'
+import {Navbar,Nav, Form, FormControl, Button, NavbarBrand} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 
 export default class search extends Component {
     constructor (props) {
         super(props)
-        this.states={}
+        this.states = {
+            activePage: 1,
+            info: undefined,
+
+        };
         this.title = this.id
         this.id = this.props.match.params;
-        this.img = this.id['id']
-        console.log(this.id)
-        console.log("HELlO")
+        this.search = this.id['id']
+
         }
 
         componentDidMount (){
             document.title = this.img;
     
             fetch(
-                'https://www.energenius.me/api/energy?name=all'
-            )
-                .then(response => response.json())
-                .then(data => {
-                    this.states['energy'] = data[0]
-                    this.setState({});
-                })
-                .catch(e => {
-                    console.log(e);
-                })
-                
-            fetch(
-                'https://www.energenius.me/api/production?name=all'
-            )
-                .then(response => response.json())
-                .then(data => {
-                    this.states['production'] = data[0]
-                    this.setState({});
-                })
-                .catch(e => {
-                    console.log(e);
-                })
-
-            fetch(
                 'https://www.energenius.me/api/country?name=all'
             )
                 .then(response => response.json())
                 .then(data => {
-                    this.states['country'] = data[0]
+                    this.states['query_result'] = data
                     this.setState({});
                 })
                 .catch(e => {
                     console.log(e);
                 })
 
-
-
-
             }
 
     render() {
+        console.log(this.states)
+
+        
+
         return (
         <div>
-            <p>{this.img}</p>
-            WENYUAN WU
+
+            {/* Reset */}
+            <Form  onSubmit={this.handleSubmit} noValidate inline className="justify-content-left col-xs-6" alignRight >
+                <Link to={'/'}>
+
+                  <Button variant="outline-primary" className="mt-2 mt-sm-0">
+                    Reset
+                  </Button>
+
+                </Link> 
+            </Form>
+
+            <p>{this.search}</p>
+
+
+
+
         </div>
         )
     }
