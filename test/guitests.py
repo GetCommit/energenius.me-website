a99f1f0ac28270ae0c816bbf22102ffd30e65cbc
Shijing Zhong <zsjgary@gmail.com>
2019-04-18 19:09:51 -0500

    Finish some part of the test

--- test/guitests.py
+++ test/guitests.py
@@ -12,36 +12,36 @@ class GUI_tests(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome('./chromedriver')
 
-    # def test_web_name(self):
-    #     home_page_name = "Energenius"
+    def test_web_name(self):
+        home_page_name = "Energenius"
         
-    #     driver = self.driver
-    #     driver.get("https://www.energenius.me/")
-    #     self.assertIn(home_page_name, driver.title)
+        driver = self.driver
+        driver.get("https://www.energenius.me/")
+        self.assertIn(home_page_name, driver.title)
 
-    # def test_navbar_Energy(self):
-    #     species_page_name = "Energy"
-    #     driver = self.driver
-    #     driver.get("https://www.energenius.me/")
-    #     species_link = driver.find_element_by_link_text('Energy')
-    #     species_link.click()
-    #     self.assertIn(species_page_name, driver.title)
+    def test_navbar_Energy(self):
+        species_page_name = "Energy"
+        driver = self.driver
+        driver.get("https://www.energenius.me/")
+        species_link = driver.find_element_by_link_text('Energy')
+        species_link.click()
+        self.assertIn(species_page_name, driver.title)
 
-    # def test_navbar_Usage(self):
-    #     location_page_name = "Production and Usage"
-    #     driver = self.driver
-    #     driver.get("https://www.energenius.me/")
-    #     location_link = driver.find_element_by_link_text('Production & Usage')
-    #     location_link.click()
-    #     self.assertIn(location_page_name, driver.title)
+    def test_navbar_Usage(self):
+        location_page_name = "Production and Usage"
+        driver = self.driver
+        driver.get("https://www.energenius.me/")
+        location_link = driver.find_element_by_link_text('Production & Usage')
+        location_link.click()
+        self.assertIn(location_page_name, driver.title)
 
-    # def test_navbar_Consumtion(self):
-    #     causes_page_name = "Country"
-    #     driver = self.driver
-    #     driver.get("https://www.energenius.me/")
-    #     causes_link = driver.find_element_by_link_text('Country')
-    #     causes_link.click()
-    #     self.assertIn(causes_page_name, driver.title)
+    def test_navbar_Consumtion(self):
+        causes_page_name = "Country"
+        driver = self.driver
+        driver.get("https://www.energenius.me/")
+        causes_link = driver.find_element_by_link_text('Country')
+        causes_link.click()
+        self.assertIn(causes_page_name, driver.title)
 
     def test_navbar_About(self):
         about_page_name = "Energenius"
@@ -53,14 +53,14 @@ class GUI_tests(unittest.TestCase):
         self.assertIn(about_page_name, driver.title)
     
     
-    # def test_Global_Search(self):
-    #     about_page_name = "undefined"
-    #     driver = self.driver
-    #     driver.get("https://www.energenius.me/")
-    #     about_link = driver.find_element_by_link_text('Global Search')
-    #     about_link.click()
-    #     print driver.title
-    #     self.assertIn(about_page_name, driver.title)
+    def test_Global_Search(self):
+        about_page_name = "undefined"
+        driver = self.driver
+        driver.get("https://www.energenius.me/")
+        about_link = driver.find_element_by_link_text('Global Search')
+        about_link.click()
+        print driver.title
+        self.assertIn(about_page_name, driver.title)
 
     
     def tearDown(self):
